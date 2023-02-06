c61dd60ef742859417670248529b6f45fccae152
James Brothers <jbrothers@flvs.net>
2023-02-06 14:21:19 -0500

    more efficient js

--- script.js
+++ script.js
@@ -7,48 +7,34 @@ A click event listener is then added to each bead element. When a bead is clicke
 The "addActives" function is then called to update the display of the game based on the values in the "actives" array. This function first removes the "active" class from all bead elements, then calculates the total by adding up the values in the "actives" array. For each active bead, the function finds the bead with the corresponding "pole-num" and "bead-num" attributes and adds the "active" class to it. Finally, the function updates the innerHTML of the element with an id of "total" with the calculated total value.*/
 
 const poles = document.querySelectorAll(".pole");
-const actives = new Array(poles.length);
+const actives = Array(poles.length).fill(0);
 
-poles.forEach((pole, index_row) => {
+poles.forEach((pole, poleIndex) => {
     const beads = pole.querySelectorAll(".bead");
-    beads.forEach((bead, index_bead) => {
-        bead.setAttribute("pole-num", poles.length - index_row - 1);
-        bead.setAttribute("bead-num", beads.length - index_bead);
+
+    beads.forEach((bead, beadIndex) => {
+        bead.setAttribute("data-pole-num", poles.length - poleIndex - 1);
+        bead.setAttribute("data-bead-num", beads.length - beadIndex);
         bead.addEventListener("click", () => {
-            bead_power = bead.getAttribute("pole-num");
-            bead_value = bead.getAttribute("bead-num");
-            if (actives[bead_power] == bead_value) {
-                if (bead_value > 1) {
-                    actives[bead_power] = actives[bead_power] - 1;
-                } else {
-                    actives[bead_power] = 0;
-                }
-            } else if (bead_value < actives[bead_power]) {
-                actives[bead_power] = bead_value - 1;
-            } else {
-                actives[bead_power] = parseInt(bead_value);
-            }
+            const polePos = +bead.getAttribute("data-pole-num");
+            const beadPos = +bead.getAttribute("data-bead-num");
+            actives[polePos] = Math.min(beadPos, actives[polePos] === beadPos ? (beadPos > 1 ? beadPos - 1 : 0) : beadPos);
             addActives();
         });
     });
 });
 
 function addActives() {
-    const beads = document.querySelectorAll(".bead");
-    beads.forEach((bead) => {
-        bead.classList.remove("active");
-    });
+    document.querySelectorAll(".bead.active").forEach(bead => bead.classList.remove("active"));
 
     let total = 0;
     actives.forEach((value, pow10) => {
-        total += Math.pow(10, pow10) * value;
+        total += 10 ** pow10 * value;
         if (value > 0) {
             document
-                .querySelector(`.bead[pole-num="${pow10}"][bead-num="${value}"]`)
+                .querySelector(`.bead[data-pole-num="${pow10}"][data-bead-num="${value}"]`)
                 .classList.add("active");
         }
     });
     document.querySelector("#total").innerHTML = total.toLocaleString();
-    // console.log(total);
 }
-
