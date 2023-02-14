1296b8721a2273a808749d30275fd7343fe42363
James Brothers <jbrothers@flvs.net>
2023-02-14 17:01:09 -0500

    gradients

--- script.js
+++ script.js
@@ -2,51 +2,51 @@ const rods = document.querySelectorAll(".rod");
 const actives = new Array(rods.length);
 
 rods.forEach((rod, index_row) => {
-  const beads = rod.querySelectorAll(".bead");
-  beads.forEach((bead, index_bead) => {
-    bead.setAttribute("data-pow10", rods.length - index_row - 1);
-    bead.setAttribute("data-value", beads.length - index_bead);
-    bead.addEventListener("click", () => {
-      bead_power = bead.getAttribute("data-pow10");
-      bead_value = bead.getAttribute("data-value");
-      if (actives[bead_power] == bead_value) {
-        if (bead_value > 1) {
-          actives[bead_power] = actives[bead_power] - 1;
-        } else {
-          actives[bead_power] = 0;
-        }
-      } else if (bead_value < actives[bead_power]) {
-        actives[bead_power] = bead_value - 1;
-      } else {
-        actives[bead_power] = parseInt(bead_value);
-      }
-      addActives();
+    const beads = rod.querySelectorAll(".bead");
+    beads.forEach((bead, index_bead) => {
+        bead.setAttribute("data-pow10", rods.length - index_row - 1);
+        bead.setAttribute("data-value", beads.length - index_bead);
+        bead.addEventListener("click", () => {
+            bead_power = bead.getAttribute("data-pow10");
+            bead_value = bead.getAttribute("data-value");
+            if (actives[bead_power] == bead_value) {
+                if (bead_value > 1) {
+                    actives[bead_power] = actives[bead_power] - 1;
+                } else {
+                    actives[bead_power] = 0;
+                }
+            } else if (bead_value < actives[bead_power]) {
+                actives[bead_power] = bead_value - 1;
+            } else {
+                actives[bead_power] = parseInt(bead_value);
+            }
+            addActives();
+        });
     });
-  });
 });
 
 function addActives() {
-  const beads = document.querySelectorAll(".bead");
-  beads.forEach((bead) => {
-    bead.classList.remove("active");
-  });
+    const beads = document.querySelectorAll(".bead");
+    beads.forEach((bead) => {
+        bead.classList.remove("active");
+    });
 
-  let total = 0;
-  actives.forEach((value, pow10) => {
-    total += Math.pow(10, pow10) * value;
-    if (value > 0) {
-      document
-        .querySelector(`.bead[data-pow10="${pow10}"][data-value="${value}"]`)
-        .classList.add("active");
-    }
-  });
-  document.querySelector("#total").innerHTML = total.toLocaleString();
-  // console.log(total);
+    let total = 0;
+    actives.forEach((value, pow10) => {
+        total += Math.pow(10, pow10) * value;
+        if (value > 0) {
+            document
+                .querySelector(`.bead[data-pow10="${pow10}"][data-value="${value}"]`)
+                .classList.add("active");
+        }
+    });
+    document.querySelector("#total").innerHTML = total.toLocaleString();
+    // console.log(total);
 }
 
 
 // Play click sound every time a bead is moved
 function play() {
-  var audio = document.getElementById("audio");
-  audio.play();
+    var audio = document.getElementById("audio");
+    audio.play();
 }
